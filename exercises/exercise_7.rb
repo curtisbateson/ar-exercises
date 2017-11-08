db86f7b85828de22217150bbe457648db838989b
Curtis Bateson <curtislbateson@gmail.com>
2017-11-08 21:40:01 +0000

    Completed final exercise

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name)
+
+new_store.errors.messages.each do |message|
+  puts message
+end
+
+new_person = Employee.create(first_name: 'Dude', last_name: 'Man-Guy', hourly_rate: 40, store_id: 47)
+
+new_person.errors.messages.each do |message|
+  puts message
+end
