9aaade26c1eec86d6a7a9b27ff7dea3d35a826fc
Curtis Bateson <curtislbateson@gmail.com>
2017-11-08 21:06:24 +0000

    Completed fifth and sixth exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: 'Curtis', last_name: 'Bateson', hourly_rate: 61)
+@store1.employees.create(first_name: 'Angus', last_name: 'Bateson', hourly_rate: 65)
+@store1.employees.create(first_name: 'Punky', last_name: 'Bateson', hourly_rate: 68)
+@store1.employees.create(first_name: 'Scruffy', last_name: 'Bateson', hourly_rate: 60)
+
+@store1.employees.create(first_name: 'Sargoplex', last_name: 'Bateson', hourly_rate: 70)
+@store1.employees.create(first_name: 'Dundee', last_name: 'Bateson', hourly_rate: 65)
+@store1.employees.create(first_name: 'Goblin King', last_name: 'Bateson', hourly_rate: 63)
